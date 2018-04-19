435b66427205b1c9725bff0ef1f6de9256ee133d
Andres Castillo-Montero <adcastillo211@gmail.com>
2018-04-19 17:06:23 -0300

    Add connection to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const todoRoutes = require('./routes/todo');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+const secrets = require('./config/secrets');
 
 app.set('view engine', 'ejs');
+
+mongoose.connect(secrets.mongodb.dbURI, () => {
+  console.log('Connected to MongoDB')
+});
 
 app.use(express.static('./public'));
 
